a79e233b0cd716f5dd1617b5b2b9f31c4ff0294e
André Abdala <andreluizabdalacosta@gmail.com>
2025-05-29 14:38:55 -0300

    Adicionando o primeiro código

---
+++ Exercícios/ola_mundo.py
@@ -0,0 +1,62 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "a9a3b259-bc6a-4d0b-a8c7-1b83879de70e",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Hello, World!\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"Hello, World!\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "31999a35-5f67-47ec-8764-36bb28ce23f0",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Mas ele tentava dizer: \"Eu te amo\"\n"
+     ]
+    }
+   ],
+   "source": [
+    "teste = \"Mas ele tentava dizer: \\\"Eu te amo\\\"\"\n",
+    "print(teste)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.10.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
